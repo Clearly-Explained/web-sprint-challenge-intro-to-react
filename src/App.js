cf8e953b8568fa33fd2aa5da155d0a5277547a23
Dionne <mylife33@hotmail.com>
2022-11-11 21:35:56 -0600

    mvp

--- src/App.js
+++ src/App.js
@@ -1,17 +1,101 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import axios from "axios";
+import Character from './components/Character';
+import styled, { keyframes } from 'styled-components';
+
+const rotate = keyframes 
+    `
+    from {
+        transform: rotate(0deg);
+    }
+    to {
+        transform: rotate(360deg);
+    }
+    `
+const Rotate = styled.div  
+    `
+    display: inline-block;
+    animation: ${rotate} 5s linear infinite;
+    padding: 2rem 1rem;
+    font-size: 1.2rem;
+    `
+const StyledImg = styled.img 
+    `
+    width: 10%;
+    height: 10%;
+    `
+const SecondStyledButton = styled.button
+  `
+    transition: all .5s ease;
+    color: blue;
+    border: 3px solid white;
+    font-family:'Montserrat', sans-serif;
+    text-transform: uppercase;
+    text-align: center;
+    line-height: 1;
+    font-size: 17px;
+    background-color : transparent grey;
+    padding: 10px;
+    outline: none;
+    border-radius: 4px;
+  :hover {
+    color: #001F3F;
+    background-color: #fff;
+  }
+  `
 
 const App = () => {
+  // const [showObjects, setShowObjects] = useState([]);
+  const [chars, setChar] = useState(null)
+  const [currentChar, setCurrentChar] = useState('1')
+  // const [moviesOrChars, setMoviesOrChars] = useState(false)
+  // const [movies, setMovies] = useState(null)
+
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
+  const openDetails = id => {
+    setCurrentChar(id)
+  }
 
   // Fetch characters from the API in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
+  useEffect(() => {
+    axios.get('https://swapi.dev/api/people')
+    .then(res => {
+      setChar(res.data)
+    })
+    .catch(err => {
+      console.log(err)
+    })
+  }, [])
+
+  // useEffect(() => {
+  //   axios.get('https://swapi.dev/api/films')
+  //   .then(res => {
+  //     setMovies(res.data)
+  //     console.log(res.data)
+  //   })
+  // }, [])
+
+  // function toggle() {
+  //   setMoviesOrChars(wasVisible => ! wasVisible);
+  // }
 
   return (
     <div className="App">
-      <h1 className="Header">Characters</h1>
+      <h1 className="Header">Star Wars Characters List</h1>
+      <Rotate><StyledImg src="https://upload.wikimedia.org/wikipedia/commons/thumb/c/ce/Star_wars2.svg/1200px-Star_wars2.svg.png"></StyledImg></Rotate>
+      {/* <SecondStyledButton onClick={toggle}> { moviesOrChars ? 'Characters' : 'Movies' } </SecondStyledButton> */}
+      {
+        chars
+        ?
+        chars.map(char => {
+          return <Character key={char.id} info={char} action={openDetails} />
+        })
+        : <p>Loading...</p>
+      }
     </div>
   );
 }
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,64 @@
 // Write your Character component here
+import React, { useState, useEffect } from "react";
+import axios from "axios"
+import styled from 'styled-components';
+
+const StyledUl = styled.ul 
+    `
+    list-style-type: none;
+    padding: 0;
+    margin: 0;
+    `
+const StyledLi = styled.li 
+    `
+    &:hover {
+        color: blue;
+    }
+    `
+const StyledDiv = styled.div
+    `
+    align-content: space-between;
+    align-items: center;
+    justify-content: center;
+    display: flex;
+    border-top: 1px solid;
+    `
+const StyledButton = styled.button 
+    `
+    margin: 50px;
+    border: none;
+    outline: none;
+    background: none;
+    `
+export default function Character({ info,elem }){
+    const [ isVisible, setIsVisible] = useState(false);
+    
+    function toggle() {
+        setIsVisible(wasVisible => ! wasVisible);
+    }
+
+
+    return (
+    <div>
+        <StyledDiv className='something'> 
+            <h3>{ info.name }</h3>
+            <StyledButton onClick={toggle}>
+                { isVisible? '^' : 'v'} 
+            </StyledButton>
+        </StyledDiv>
+        {
+            isVisible && (
+                <StyledUl>
+                    <StyledLi>Gender: { info.gender }</StyledLi>
+                    <StyledLi>Height: { info.height }</StyledLi>
+                    <StyledLi>Mass: { info.mass }</StyledLi>
+                    <StyledLi>Birth Year: { info.birth_year }</StyledLi>
+                    <StyledLi>Eye Color: { info.eye_color }</StyledLi>
+                    <StyledLi>Hair Color: { info.hair_color }</StyledLi>
+                    <StyledLi>Skin Color: { info.skin_color }</StyledLi>
+                </StyledUl>
+            )
+        }
+    </div>
+    )
+}
